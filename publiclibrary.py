5d4a5b9b0b9f2c2acbf9ff00589784d4411f8464
Constantine <constantine.hatzis@gmail.com>
2015-01-02 11:54:02 -0800

    Defined name setter and getter properties for Library.

--- publiclibrary.py
+++ publiclibrary.py
@@ -4,7 +4,16 @@ from __future__ import unicode_literals
 
 
 class Library(object):
-    pass
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
 
 
 class Shelf(object):
