e86ed194104bd18c920cae791d1b47a086be734a
Constantine <constantine.hatzis@gmail.com>
2015-01-02 11:50:04 -0800

    Defined name setter and getter properties for Book.

--- publiclibrary.py
+++ publiclibrary.py
@@ -12,4 +12,14 @@ class Shelf(object):
 
 
 class Book(object):
-    pass
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
