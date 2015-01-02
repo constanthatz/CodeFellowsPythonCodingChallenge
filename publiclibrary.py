55277740e9683b0eff070670e2cdb0dd067dfc9b
Constantine <constantine.hatzis@gmail.com>
2015-01-02 11:42:47 -0800

    Initialized LIbrary, Shelf, and Book classes.

--- publiclibrary.py
+++ publiclibrary.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from __future__ import unicode_literals
+
+
+class Library(object):
+    pass
+
+
+class Shelf(object):
+    pass
+
+
+class Book(object):
+    pass
